b8938e317dfed84a5c0f504166f9e766905ecd16
bengkoongoh <bengkoon.goh27@gmail.com>
2022-03-28 16:36:09 +0800

    added types code

--- typesInGo.go
+++ typesInGo.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
+
 //Insert variables declarations here based on activity
 
 func tellMeTypes() {
 	//insert code here to print out types of variables
+	text := "The following is the account information"
+	credit := 123.55
+
+	fmt.Printf("%T %T", text, credit)
+
 }
 
 func main() {
