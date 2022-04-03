092eaf108a3c030ec2eec9ff4ea33b318488aac2
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 10:20:10 +0800

    print value and type of variables

--- typesInGo.go
+++ typesInGo.go
@@ -7,10 +7,26 @@ import "fmt"
 func tellMeTypes() {
 	//insert code here to print out types of variables
 	text := "The following is the account information"
+	firstName := "Luke"
+	lastName := "Skywalkter"
+	age := 20
+	weight := 73
+	height := 1.72
 	credit := 123.55
+	accountName := "admin"
+	accountPassword := "password"
+	isSubscribed := true
 
-	fmt.Printf("%T %T", text, credit)
-
+	fmt.Printf("value: %v, type: %T \n", text, text)
+	fmt.Printf("value: %v, type: %T \n", firstName, firstName)
+	fmt.Printf("value: %v, type: %T \n", lastName, lastName)
+	fmt.Printf("value: %v, type: %T \n", age, age)
+	fmt.Printf("value: %v, type: %T \n", weight, weight)
+	fmt.Printf("value: %v, type: %T \n", height, height)
+	fmt.Printf("value: %v, type: %T \n", credit, credit)
+	fmt.Printf("value: %v, type: %T \n", accountName, accountName)
+	fmt.Printf("value: %v, type: %T \n", accountPassword, accountPassword)
+	fmt.Printf("value: %v, type: %T \n", isSubscribed, isSubscribed)
 }
 
 func main() {
